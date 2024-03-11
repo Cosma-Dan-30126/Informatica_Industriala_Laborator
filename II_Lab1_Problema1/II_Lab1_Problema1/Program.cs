1e20bc1a149d7dad544fb18f7027a5c4a23603af
Cosma-Dan-30126 <126558035+Cosma-Dan-30126@users.noreply.github.com>
2024-03-11 20:26:03 +0200

    Add files via upload

---
+++ II_Lab1_Problema1/II_Lab1_Problema1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Dati valoarea n de parametri care vor fi afisati de firul Fibonacci: ");
+        int n = Convert.ToInt32(Console.ReadLine()) ;
+        int f0 = 0;
+        int f1 = 1;
+        for(int i=2;i<=n;i++)
+        {
+            int fn = f0 + f1;
+            Console.Write(fn + " ");
+            f0 = f1;
+            f1 = fn;
+        }
+        Console.ReadLine();
+    }
+}
+
