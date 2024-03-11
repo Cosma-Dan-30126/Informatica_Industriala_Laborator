1364c13b0652b794909c9e80ea58b611b74aa13a
Cosma-Dan-30126 <126558035+Cosma-Dan-30126@users.noreply.github.com>
2024-03-11 20:32:54 +0200

    Windows form app3

---
+++ II_Lab2_p3/II_Lab2_p3/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace II_Lab2_p3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.adunareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scadereToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inmultireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.impartireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(104, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(104, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(104, 211);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adunareToolStripMenuItem,
+            this.scadereToolStripMenuItem,
+            this.inmultireToolStripMenuItem,
+            this.impartireToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // adunareToolStripMenuItem
+            // 
+            this.adunareToolStripMenuItem.Name = "adunareToolStripMenuItem";
+            this.adunareToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.adunareToolStripMenuItem.Text = "Adunare";
+            this.adunareToolStripMenuItem.Click += new System.EventHandler(this.adunareToolStripMenuItem_Click);
+            // 
+            // scadereToolStripMenuItem
+            // 
+            this.scadereToolStripMenuItem.Name = "scadereToolStripMenuItem";
+            this.scadereToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.scadereToolStripMenuItem.Text = "Scadere";
+            this.scadereToolStripMenuItem.Click += new System.EventHandler(this.scadereToolStripMenuItem_Click);
+            // 
+            // inmultireToolStripMenuItem
+            // 
+            this.inmultireToolStripMenuItem.Name = "inmultireToolStripMenuItem";
+            this.inmultireToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.inmultireToolStripMenuItem.Text = "Inmultire";
+            this.inmultireToolStripMenuItem.Click += new System.EventHandler(this.inmultireToolStripMenuItem_Click);
+            // 
+            // impartireToolStripMenuItem
+            // 
+            this.impartireToolStripMenuItem.Name = "impartireToolStripMenuItem";
+            this.impartireToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.impartireToolStripMenuItem.Text = "Impartire";
+            this.impartireToolStripMenuItem.Click += new System.EventHandler(this.impartireToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Caseta1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Caseta2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Rezultat";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Location = new System.Drawing.Point(663, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Exit!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 394);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem adunareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scadereToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inmultireToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem impartireToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ II_Lab2_p3/II_Lab2_p3/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace II_Lab2_p3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void adunareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double nr1) && double.TryParse(textBox2.Text, out double nr2))
+            {
+                double rezultat = nr1 + nr2;
+                textBox3.Text = rezultat.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Numerele nu corespund cerintei date!!!");
+            }
+
+        }
+
+        private void scadereToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double nr1) && double.TryParse(textBox2.Text, out double nr2))
+            {
+                double rezultat = nr1 - nr2;
+                textBox3.Text = rezultat.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Numerele nu corespund cerintei date!!!");
+            }
+
+        }
+
+        private void inmultireToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double nr1) && double.TryParse(textBox2.Text, out double nr2))
+            {
+                double rezultat = nr1 * nr2;
+                textBox3.Text = rezultat.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Numerele nu corespund cerintei date!!!");
+
+            }
+
+
+        }
+
+        private void impartireToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double nr1) && double.TryParse(textBox2.Text, out double nr2))
+            {
+                if (nr2 != 0)
+                {
+                    double rezultat = nr1 / nr2;
+                    textBox3.Text = rezultat.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Nu se poate împărți la zero.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Numerele nu corespund cerintei date!!!");
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+       
+}
